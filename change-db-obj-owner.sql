fca2ad417761f108cbacf0f604b8a0ac3ee37218
debatosh99 <63183667+debatosh99@users.noreply.github.com>
2025-04-24 12:46:17 +0530

    Update change-db-obj-owner.sql

--- change-db-obj-owner.sql
+++ change-db-obj-owner.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION change_schema_objects_owner(schema_name TEXT, new_owner TEXT)
+CREATE OR REPLACE FUNCTION change_schema_objects_owner(schema_name TEXT, new_owner TEXT)
 RETURNS void AS $$
 DECLARE
     object_name TEXT;
@@ -10,11 +10,14 @@ BEGIN
         SELECT view_name, 'VIEW' FROM information_schema.views WHERE table_schema = schema_name
         -- Add other object types as needed (e.g., functions, sequences)
     LOOP
-        EXECUTE format('ALTER %s %I.%I OWNER TO %I',
-                       object_type, schema_name, object_name, new_owner);
+        BEGIN
+            EXECUTE format('ALTER %s %I.%I OWNER TO %I',
+                           object_type, schema_name, object_name, new_owner);
+            RAISE NOTICE 'Changed owner of %s %I.%I to %I', object_type, schema_name, object_name, new_owner;
+        EXCEPTION WHEN insufficient_privilege THEN
+            RAISE NOTICE 'Insufficient privileges to change owner of %s %I.%I', object_type, schema_name, object_name;
+            -- Log the error or take other appropriate action (e.g., skip, retry)
+        END;
     END LOOP;
 END;
 $$ LANGUAGE plpgsql;
-
--- Example usage:
-SELECT change_schema_objects_owner('my_schema', 'new_user');
