c636f1e669aa87b8855932c863eb39c053bb0c14
debatosh99 <63183667+debatosh99@users.noreply.github.com>
2026-03-04 19:29:31 +0530

    Add files via upload

---
+++ pyiceberg_cred_vend_mode_with_sa_works.py
@@ -0,0 +1,85 @@
+#######################################################################
+# FIRST use: gcloud auth application-default login (doubtful)
+# run this file in gcp shell editor
+# worked when simply opened cloud shell editor and simply ran this python file without even doing gcloud auth application-default login
+#######################################################################
+
+import pyarrow as pa
+import google.auth
+from pyiceberg.catalog import load_catalog
+from pyiceberg.schema import Schema
+from pyiceberg.types import NestedField, StringType, IntegerType
+
+# 1. Fetch Credentials safely
+# 'google.auth.default' will automatically find your 'gcloud auth' credentials
+credentials, project_id = google.auth.default()
+
+# Robust way to ensure token is present without manual refresh crashes
+from google.auth.transport.requests import Request
+
+# if not credentials.token:
+#     credentials.refresh(Request())
+# auth_token = credentials.token
+
+auth_token = ""
+
+# 2. Configure the BigLake REST Catalog
+REGION = "us-central1"
+CATALOG_NAME = "learnbiglakeiceberg20"
+WAREHOUSE_PATH = "gs://learnbiglakeiceberg20/"
+
+catalog = load_catalog(
+    CATALOG_NAME,
+    **{
+        "type": "rest",
+        "uri": f"https://biglake.googleapis.com/iceberg/v1/restcatalog",
+        "token": auth_token,
+        "warehouse": WAREHOUSE_PATH,
+        "header.X-Goog-User-Project": "trim-strength-477307-h0",
+        "header.Authorization": f"Bearer {auth_token}",
+        "py-io-impl": "pyiceberg.io.pyarrow.PyArrowFileIO",
+    }
+)
+
+# # 3. Define Schema & Table
+# schema = Schema(
+#     NestedField(field_id=1, name="id", field_type=IntegerType(), required=True),
+#     NestedField(field_id=2, name="data", field_type=StringType(), required=False),
+# )
+
+namespace = "iceberg_rest_catalog_namespace20"
+table_identifier = f"{namespace}.customer_details_pyiceberg_created"
+
+# # Create namespace and table if they don't exist
+# if namespace not in catalog.list_namespaces():
+#     catalog.create_namespace_if_not_exists(namespace)
+
+# try:
+#     table = catalog.load_table(table_identifier)
+# except Exception:
+#     table = catalog.create_table_if_not_exists(
+#         identifier=table_identifier,
+#         schema=schema,
+#         location=f"{WAREHOUSE_PATH}{namespace}/my_pyiceberg_table2"
+#     )
+
+arrow_schema = pa.schema([
+    pa.field("id", pa.int32(), nullable=False),
+    pa.field("data", pa.string(), nullable=True)
+])
+
+# 2. Create the data using this schema
+data_rows = [
+    {"id": 3, "data": "Hello BigLake2"},
+    {"id": 4, "data": "Type mismatch fixed2"}
+]
+
+data = pa.Table.from_pylist(data_rows, schema=arrow_schema)
+table = catalog.load_table(table_identifier)
+table.append(data)
+print(f"Successfully wrote {len(data)} records to {table_identifier}")
+
+
+#table = catalog.load_table(table_identifier)
+result_arrow = table.scan().to_arrow()
+print(result_arrow.to_pandas())
